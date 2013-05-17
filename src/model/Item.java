4bf9867e9b4197ad8dbe1d335de5c642a320db27
Andrea Barillari <andrea.barillari@gmail.com>
2013-05-17 12:11:52 +0200

    Added model and json package

---
+++ src/model/Item.java
@@ -0,0 +1,131 @@
+package model;
+
+public class Item {
+	private Integer completeness;
+	private String[] dataProvider;
+	private String[] europeanaCollectionName;
+	private String id;
+	private String guid;
+	private String link;
+	private String provider;
+	private String[] rights;
+	private String type;
+	private String[] dcCreator;
+	private String edmConceptLabel;
+	private String edmPreview;
+	private String edmTimespanLabel;
+	private Integer europeanaCompleteness;
+	private String[] language;
+	private String title;
+	private String year;
+	
+	public Item(){
+		
+	}
+	
+	
+	
+	public Integer getCompleteness() {
+		return completeness;
+	}
+	public void setCompleteness(Integer completeness) {
+		this.completeness = completeness;
+	}
+	public String[] getDataProvider() {
+		return dataProvider;
+	}
+	public void setDataProvider(String[] dataProvider) {
+		this.dataProvider = dataProvider;
+	}
+	public String[] getEuropeanaCollectionName() {
+		return europeanaCollectionName;
+	}
+	public void setEuropeanaCollectionName(String[] europeanaCollectionName) {
+		this.europeanaCollectionName = europeanaCollectionName;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getGuid() {
+		return guid;
+	}
+	public void setGuid(String guid) {
+		this.guid = guid;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getProvider() {
+		return provider;
+	}
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+	public String[] getRights() {
+		return rights;
+	}
+	public void setRights(String[] rights) {
+		this.rights = rights;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String[] getDcCreator() {
+		return dcCreator;
+	}
+	public void setDcCreator(String[] dcCreator) {
+		this.dcCreator = dcCreator;
+	}
+	public String getEdmConceptLabel() {
+		return edmConceptLabel;
+	}
+	public void setEdmConceptLabel(String edmConceptLabel) {
+		this.edmConceptLabel = edmConceptLabel;
+	}
+	public String getEdmPreview() {
+		return edmPreview;
+	}
+	public void setEdmPreview(String edmPreview) {
+		this.edmPreview = edmPreview;
+	}
+	public String getEdmTimespanLabel() {
+		return edmTimespanLabel;
+	}
+	public void setEdmTimespanLabel(String edmTimespanLabel) {
+		this.edmTimespanLabel = edmTimespanLabel;
+	}
+	public Integer getEuropeanaCompleteness() {
+		return europeanaCompleteness;
+	}
+	public void setEuropeanaCompleteness(Integer europeanaCompleteness) {
+		this.europeanaCompleteness = europeanaCompleteness;
+	}
+	public String[] getLanguage() {
+		return language;
+	}
+	public void setLanguage(String[] language) {
+		this.language = language;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	
+}
