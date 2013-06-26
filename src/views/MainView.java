a00c9abd74c74a5fcc0e0d19799f7ee7fb563966
Andrea Barillari <andrea.barillari@gmail.com>
2013-06-26 20:07:13 +0200

    commit to merge

--- src/views/MainView.java
+++ src/views/MainView.java
@@ -1,8 +1,11 @@
 package views;
 
+import org.vaadin.risto.stepper.IntStepper;
+
 import com.vaadin.ui.Button;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.Panel;
+import com.vaadin.ui.Select;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
@@ -15,11 +18,15 @@ public class MainView extends Window {
 	 */
 	private static final long serialVersionUID = -6826161044686784696L;
 
+	
+
 	private VerticalLayout mainLayout;
 	private Panel mainPanel;
 	private GridLayout panelLayout;
 	private TextField textfield;
 	private Button searchButton;
+	private IntStepper stepper;
+	private Select typeSelect;
 	
 	public MainView(){
 		super("Europeana Crawler");
@@ -36,22 +43,36 @@ public class MainView extends Window {
 		this.setWidth("100%");
 		this.setHeight("100%");
 		
-		this.panelLayout = new GridLayout(1, 2);
+		this.panelLayout = new GridLayout(2, 3);
 		this.panelLayout.setSizeFull();
+		this.panelLayout.setSpacing(true);
 		
 		this.textfield = new TextField();
 		this.textfield.setWidth("50%");
 		this.textfield.setImmediate(true);
 		
+		this.stepper = new IntStepper("N° result");
+		stepper.setMinValue(1);
+		stepper.setValue(1);
+		stepper.setImmediate(true);
+		
+		this.typeSelect = new Select("Type");
+		
 		
 		this.searchButton = new Button("Search");
 		
-		this.panelLayout.addComponent(textfield, 0, 0);
-		this.panelLayout.addComponent(searchButton, 0, 1);	
+		this.panelLayout.addComponent(textfield, 0, 0, 1, 0);
+		this.panelLayout.addComponent(stepper, 0, 1);	
+		this.panelLayout.addComponent(searchButton, 0, 2);	
+		
 		
 		this.mainPanel.setContent(panelLayout);
 		this.mainLayout.addComponent(mainPanel);
 		this.setContent(mainLayout);
+		
+	}
+	
+	private void initTypeSelect(){
 		
 	}
 	
@@ -69,6 +90,22 @@ public class MainView extends Window {
 
 	public void setSearchButton(Button searchButton) {
 		this.searchButton = searchButton;
+	}
+	
+	public IntStepper getStepper() {
+		return stepper;
+	}
+
+	public void setStepper(IntStepper stepper) {
+		this.stepper = stepper;
+	}
+
+	public Select getTypeSelect() {
+		return typeSelect;
+	}
+
+	public void setTypeSelect(Select typeSelect) {
+		this.typeSelect = typeSelect;
 	}
 	
 }
