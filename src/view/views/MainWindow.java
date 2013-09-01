8d904b6bce165b5abbc48c5e3cae38017ee8a52e
Andrea Barillari <andrea.barillari@gmail.com>
2013-09-01 11:46:57 +0200

    Add Main Window class

---
+++ src/view/views/MainWindow.java
@@ -0,0 +1,20 @@
+package view.views;
+
+import com.vaadin.ui.Window;
+
+public class MainWindow extends Window {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7008709578000661192L;
+
+	
+	public MainWindow(){
+		super("Multimedia Data Crawler");
+		this.setScrollable(true);
+		this.setSizeFull();
+	}
+	
+
+}
